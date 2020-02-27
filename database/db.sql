96c47ac05f6f763c350e600eaa111729ebbfc319
Alejandro Barba <alejandrobarba742@gmail.com>
2020-02-27 01:01:08 -0600

    sentencias sql

---
+++ database/db.sql
@@ -0,0 +1,45 @@
+CREATE TABLE estudiantes(
+    id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    centro_id INTEGER UNSIGNED
+);
+
+CREATE TABLE asesores(
+    id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    centro_id INTEGER UNSIGNED,
+    clave INTEGER UNSIGNED
+);
+
+CREATE TABLE citas(
+        id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        asesor_id INTEGER UNSIGNED,
+        estudiante_id INTEGER UNSIGNED,
+        materia VARCHAR(100) NOT NULL,
+        lugar VARCHAR(100) NOT NULL,
+        fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE centros(
+    id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(120) NOT NULL UNIQUE
+);
+
+ALTER TABLE estudiantes
+ADD CONSTRAINT fk_centro 
+FOREIGN KEY (centro_id)
+REFERENCES centros(id);
+
+ALTER TABLE citas
+ADD CONSTRAINT fk_asesor 
+FOREIGN KEY (asesor_id)
+REFERENCES asesores(id);
+
+ALTER TABLE citas
+ADD CONSTRAINT fk_estudiante 
+FOREIGN KEY (estudiante_id)
+REFERENCES estudiantes(id);
