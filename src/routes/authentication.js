dda13fd5b233a05e14dbf1de3e90e25ca135799c
Alejandro Barba <alejandrobarba742@gmail.com>
2020-03-02 21:49:50 -0600

    Llamar a passport en la ruta

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
 const router = express.Router();
-const pool = require('../database');
+const passport = require('passport');
 
-router.get('/auth',(req,res)=>{
-    res.send('GET AUTH');
+
+router.get('/signup',(req,res)=>{
+    res.render('auth/signup');
 });
 
-
+router.post('/signup',passport.authenticate('local.signup',{
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+router.get('/profile',(req,res)=>{
+    res.send('Profile');
+});
 
 module.exports = router;
