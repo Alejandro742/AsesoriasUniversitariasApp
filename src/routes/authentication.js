bd630be972506ea4aa673f55af5a16907f2cbe3c
Alejandro Barba <alejandrobarba742@gmail.com>
2020-04-30 17:13:37 -0500

    ruta para loggin

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -13,6 +13,21 @@ router.post('/signup',passport.authenticate('local.signup',{
     failureRedirect: '/signup',
     failureFlash: true
 }));
+
+
+router.get('/signin',(req,res)=>{
+    res.render('auth/signin');
+});
+
+router.post('/signin',(passport.authenticate('local.signin',{
+    successRedirect:"/profile",
+    failureRedirect:'/signin',
+    failureFlash:true,
+})));
+
+
+
+
 router.get('/profile',(req,res)=>{
     res.send('Profile');
 });
