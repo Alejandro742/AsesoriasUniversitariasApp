eb19599a171b48872a0c7399e45e67359e071e77
Alejandro Barba <alejandrobarba742@gmail.com>
2020-04-30 17:13:28 -0500

    Código en passport par login

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -7,6 +7,31 @@ const LocalStrategy = require('passport-local').Strategy;
 const pool = require('../database');
 const helpers = require('../lib/helpers');
 
+passport.use('local.signin',new LocalStrategy({
+    usernameField:"nombre",
+    passwordField:"password",
+    passReqToCallback:true
+},async(req,nombre,password,done)=>{
+
+    const rows = await pool.query(`SELECT * FROM estudiantes WHERE email = "${req.body.email}"`);
+    if(rows){
+        const user = rows[0];
+        const isValidPassword = await helpers.matchPassword(req.body.password,user.password);
+        if(isValidPassword){
+            //contra correcta
+            done(null,user,req.flash('success','Welcome'+ user.nombre));
+        }
+        else{
+            //contra incorrecta
+            done(null,false,req.flash("message","Contraseña incorrecta"));
+        }
+    }
+    else{
+        //No se encontró en db
+        return done(null,false,req.flash("message","Correo no existe"));
+    }
+}));
+
 passport.use('local.signup',new LocalStrategy({
     usernameField: 'nombre',
     passwordField: 'password',
